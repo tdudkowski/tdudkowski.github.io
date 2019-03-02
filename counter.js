cf3619bee2413764bb71d94f8036953b5df50f05
Tadeusz Dudkowski <43390633+tdudkowski@users.noreply.github.com>
2019-03-02 18:05:45 +0100

    divide into functions

--- counter.js
+++ counter.js
@@ -19,27 +19,54 @@
 // }
 // todayDay = todayDay + new Date().getDate(); // add number of days from this month
 
+// UTILITIES
 const counter = document.querySelector('.counter span');
 // number of days in months
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-// start day declaration; first numer is a month, second one is a day
-const startDay = '02-09';
 //reducer function
 const add = function (accumulator, a) {
  return accumulator + a;
 }
-// number of days till start day
-let startDateMonths = Number(startDay.slice(0, 2));
-startDateMonths > 1 ? startDateMonths = months.slice(0, startDateMonths - 1).reduce(add) : startDateMonths = 0;
-let startDateDays = Number(startDay.slice(3));
-startDateDays > 1 ? startDateDays : startDateDays = 0;
-const startDate = startDateMonths + startDateDays;
-// number of days till today
-let daysTillToday = new Date().getMonth(); // find number of previous months
-daysTillToday = months.slice(0, daysTillToday).reduce(add); // number of days in previous months (if they are)
-daysTillToday += new Date().getDate(); // plus days from this month
+// function counting number of days till specified date
+const countDays = (day) => {
+ let dateMonths = Number(day.slice(0, 2));
+ dateMonths > 1 ? dateMonths = months.slice(0, dateMonths - 1).reduce(add) : dateMonths = 0; // conting number of days from previous months
+ let dateDays = Number(day.slice(3));
+ dateDays = 1 && dateMonths == 0 ? dateDays = 0 : dateDays; // removing January 1st from counting
+ const numberOfDays = dateMonths + dateDays;
+ return numberOfDays;
+}
+// function creating date format
+const formatDate = (month, day) => {
+ const formattedDate = `${month>9?month:'0'+month}-${day>9?day:'0'+day}`;
+ return formattedDate;
+}
+
+// START DAY
+// start day declaration
+const monthsStart = 2;
+const daysStart = 9;
+const startDay = formatDate(monthsStart, daysStart);
+// number of days till start day - daysTillStart
+daysTillStart = countDays(startDay);
+
+// END DAY
+// end day declaration; first number is a month, second one is a day
+let endDay = '03-02';
+// if endDay is today; if not - comment 3 lines below
+const monthsEnd = new Date().getMonth() + 1;
+const daysEnd = new Date().getDate();
+endDay = formatDate(monthsEnd, daysEnd);
+// number of days till start day - daysTillEnd
+daysTillEnd = countDays(endDay);
+// old way
+// let daysTillEnd = new Date().getMonth(); // find number of previous months
+// daysTillEnd = months.slice(0, daysTillEnd).reduce(add); // number of days in previous months (if they are)
+// daysTillEnd += new Date().getDate(); // plus days from this month
+
+// DIFFERENCE
 // count difference
-const numberOfDays = daysTillToday - startDate;
+const result = daysTillEnd - daysTillStart;
 // show it
-counter.textContent = numberOfDays;
+counter.textContent = result;
   
